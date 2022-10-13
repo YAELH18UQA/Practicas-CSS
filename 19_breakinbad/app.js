eb45cb51201460cabe084145b889e9435a3c07e5
Heydan Yael <hesh050918hmcrnya2@soycecytem.mx>
2022-10-13 07:49:49 -0500

    Update

--- 19_breakinbad/app.js
+++ 19_breakinbad/app.js
@@ -1,10 +1,10 @@
-const obtenerFrase=async ()=>{
-    const api=await fetch("https://api.breakingbadquotes.xyz/v1/quotes");
-    const json=await api.json();
-    console.log(json);
-    let frase=document.querySelector("#frase");
-    frase.innerHTML=`
-    <h1>${json[0].quote}</h1>
-    <p>-${json[0].author}</p>`;
-
+const consultarAPI = async () =>{
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+   let f=frase[0];
+   let divFrase=document.querySelector("#frase");
+   divFrase.innerHTML=`
+       <h1>${f.quote} </h1>
+       <p>${f.author}</p>
+`;
 }
